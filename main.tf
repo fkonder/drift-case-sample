1e675169d0e4269499c648d6db5c5f6f65f5f240
Ömer Faruk <omerfaruk@veloxity.net>
2025-04-17 20:58:03 +0300

    deleted other vm

--- main.tf
+++ main.tf
@@ -10,19 +10,3 @@ provider "google" {
   region  = "europe-west1"
 }
 
-resource "google_compute_instance" "example" {
-  name         = "example-instance"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
-}
