653fcf779a391c3df90ae1ccf7c4bd1523f52a3d
Ömer Faruk <omerfaruk@veloxity.net>
2025-04-17 16:52:29 +0300

    added gcs for tfstate file

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket  = "fkonder-drift-case"
+    prefix  = "drift-detect-case/terraform/state"
+  }
+}
+
 provider "google" {
   project = "fkonder"
   region  = "europe-west1"
